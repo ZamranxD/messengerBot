246227a97cb30186d5e3b849ca4ff34f69603358
ZamranxD <zamran.butt.1@gmail.com>
2022-08-01 01:13:42 +0500

    Update element selectors

--- fbot.py
+++ fbot.py
@@ -1,5 +1,6 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 chrome_options = webdriver.ChromeOptions()
@@ -14,18 +15,18 @@ driver.get("https://web.facebook.com/messages/t/ZamranxD")
 email = "email"
 password = "pass"
 login = "loginbutton"
-message = "//*[@id='js_11']"
-send = "/html/body/div[1]/div[3]/div[1]/div/div/div/div[2]/span/div[2]/div[2]/div[2]/div[2]/a"
+message = '//*[@id="mount_0_0_Hi"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[4]/div[2]/div/div/div'
+send = '//*[@id="mount_0_0_x7"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/div'
 
-semail = driver.find_element_by_id(email)
+semail = driver.find_element(By.ID, email)
 #you need to enter your email or phone-number here to login
-semail.send_keys("email@example.com")
+semail.send_keys("example@example.com")
 
-spassword = driver.find_element_by_id(password)
+spassword = driver.find_element(By.ID, password)
 #password goes here
-spassword.send_keys("password")
+spassword.send_keys("dummypass")
 
-slogin = driver.find_element_by_id(login)
+slogin = driver.find_element(By.ID, login)
 slogin.click()
 
 time.sleep(2)
@@ -33,9 +34,9 @@ time.sleep(2)
 counter = 0
 
 while counter < 10: #set this value to the number of messages you wanna send. If you want to send 100 messages, write counter < 100
-    smessage = driver.find_element_by_xpath(message)
+    smessage = driver.find_element(By.XPATH, message)   #this is not working as of now for some reason - might be the wrong Xpath or im prolly selecting the wrong element's Xpath
     smessage.send_keys("Hi! I am a Bot") #change this text to whatever you wanna send
-    ssend = driver.find_element_by_xpath(send)
+    ssend = driver.find_element(By.XPATH, send)
     ssend.click()
     counter += 1
 
